4d556313a7b049a2ac3ed8f1bec19996337cfe50
Thomas Bird <thomas.bird1984@gmail.com>
2024-01-13 02:20:59 -0500

    feat: added social bar

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,11 +1,57 @@
-import {Button, Container, Grid} from "@mui/material";
+import {Button, Container, Grid, IconButton, Tooltip} from "@mui/material";
 import cn from "classnames";
 import styles from './Hero.module.scss';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import XIcon from '@mui/icons-material/X';
+import GitHubIcon from "@mui/icons-material/GitHub";
+import EventIcon from '@mui/icons-material/Event';
+import ContactPageIcon from '@mui/icons-material/ContactPage';
+import MailOutlineIcon from '@mui/icons-material/MailOutline';
 
 const Hero = () => {
   return (
     <Container maxWidth={false} disableGutters className={styles.hero}>
       <Container>
+        <Grid container>
+          <div className={styles.socials}>
+            <Tooltip title="LinkedIn">
+              <IconButton className={styles.icons}>
+                <LinkedInIcon />
+              </IconButton>
+            </Tooltip>
+            <Tooltip title="Resume">
+              <IconButton className={styles.icons}>
+                <ContactPageIcon />
+              </IconButton>
+            </Tooltip>
+            <Tooltip title="Contact me">
+              <IconButton className={styles.icons}>
+                <MailOutlineIcon />
+              </IconButton>
+            </Tooltip>
+            <Tooltip title="Github">
+              <IconButton className={styles.icons}>
+                <GitHubIcon />
+              </IconButton>
+            </Tooltip>
+            <Tooltip title="Calendly">
+              <IconButton className={styles.icons}>
+                <EventIcon />
+              </IconButton>
+            </Tooltip>
+            <Tooltip title="Instagram">
+              <IconButton className={styles.icons}>
+                <InstagramIcon />
+              </IconButton>
+            </Tooltip>
+            <Tooltip title="Twitter/X">
+              <IconButton className={styles.icons}>
+                <XIcon />
+              </IconButton>
+            </Tooltip>
+          </div>
+        </Grid>
         <Grid container spacing={2}>
           <Grid item xs={12} md={6} className={styles.heroImage}>
             <img src="/thomas-bird.png" className={cn(styles.heroImageItem, 'animate__animated animate__slow animate__fadeIn ')} alt="Thomas Bird" />
