e1d70cc06b35dd1df1dab92397a213dde131a85e
Thomas Bird <thomas.bird1984@gmail.com>
2024-01-15 03:16:41 -0500

    feat: adding animation to PageSectionTitle

--- src/components/PageSectionTitle/PageSectionTitle.tsx
+++ src/components/PageSectionTitle/PageSectionTitle.tsx
@@ -1,18 +1,37 @@
 import styles from './PageSectionTitle.module.scss';
-import {Grid} from "@mui/material";
+import { motion } from 'framer-motion';
 
 type PageSectionTitleProps = {
   title: string;
 }
 
+const item = {
+  hidden: { y: 20, opacity: 0 },
+  visible: (i: number) => ({
+    y: 0,
+    opacity: 1,
+    transition: {
+      delay: i * 0.3,
+      duration: i * 0.2
+    }
+  }),
+};
+
 // todo: add motion to here
 const PageSectionTitle = ({ title }: PageSectionTitleProps) => (
-  <Grid className={styles.pageTitle}>
-    <h2 className={styles.title}>
+  <motion.div
+    className={styles.pageTitle}
+    initial="hidden"
+    whileInView="visible"
+    viewport={{
+      amount: 0.5
+    }}
+  >
+    <motion.h2 custom={2} variants={item} className={styles.title}>
       {title}
-    </h2>
-    <div className={styles.titleBorder}></div>
-  </Grid>
+    </motion.h2>
+    <motion.div custom={3} variants={item} className={styles.titleBorder}></motion.div>
+  </motion.div>
 )
 
 export default PageSectionTitle;
