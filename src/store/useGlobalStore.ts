55267e235d27b8c7721214acc893feb5997bfd12
Thomas Bird <thomas.bird@c-openai.com>
2024-10-13 20:32:13 -0400

    fix: store

--- src/store/useGlobalStore.ts
+++ src/store/useGlobalStore.ts
@@ -1,9 +1,10 @@
 import {createGlobalStoreSlice} from "@/store/slices/createGlobalStoreSlice";
 import {createJSONStorage, persist} from "zustand/middleware";
 import {create} from "zustand";
+import {immer} from "zustand/middleware/immer";
 
 export const useGlobalStore = create(persist(
-  createGlobalStoreSlice,
+  immer(createGlobalStoreSlice),
   {
     name: 'global-storage',
     storage: createJSONStorage(() => sessionStorage),
