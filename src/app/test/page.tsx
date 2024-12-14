501bde6201a5cd4c58376f248cc5b2c73bd6a326
Thomas Bird <thomas.bird@c-openai.com>
2024-12-14 13:03:22 -0500

    feat: integrate react-markdown

---
+++ src/app/test/page.tsx
@@ -0,0 +1,43 @@
+'use client';
+
+import client from "@/services/api";
+import {useEffect, useState} from "react";
+import styles from '../page.module.scss'
+import {Container} from '@mui/material';
+import Hero from '@/components/Hero/Hero';
+import Box from '@mui/material/Box';
+
+import Markdown from 'react-markdown';
+import rehypeHighlight from 'rehype-highlight'
+import SectionContainer from "@/components/SectionContainer/SectionContainer";
+
+const Services = () => {
+  const [post, setPost] = useState(undefined);
+  useEffect(() => {
+    const requestPost = async () => {
+      try {
+        const res = await client.getEntry('1SfRwXUWegPMcpc2bImYIM');
+
+        console.log('res', res);
+      } catch (e) {
+        throw new Error(e as any);
+      } finally {
+
+      }
+    }
+
+    requestPost()
+  }, [])
+  return (
+    <Container className={styles.overallWrapper} maxWidth={false} disableGutters>
+      <Hero navOnly={true} />
+      <SectionContainer styleName={styles.wrapper}>
+      <Box>
+        {post && <Markdown rehypePlugins={[rehypeHighlight]}>{post}</Markdown>}
+      </Box>
+      </SectionContainer>
+    </Container>
+  )
+}
+
+export default Services;
