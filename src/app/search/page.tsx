eb6a1d23acb106daa7eb735dd5ca1339d37ee5e0
Thomas Bird <thomas.bird@c-openai.com>
2025-01-24 00:41:22 -0500

    feature: got search working but now need to button everything up

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -3,28 +3,30 @@
 import {Helmet} from "react-helmet";
 import SectionContainer from "@/components/SectionContainer/SectionContainer";
 import styles from "@/app/p/[slug]/page.module.scss";
-import {Container, Grid, Stack} from "@mui/material";
+import {Container, Skeleton, Stack} from "@mui/material";
 import Button from "@mui/material/Button";
-import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
-import SkeletonSwitcher from "@/components/SkeletonSwitcher/SkeletonSwitcher";
 import Typography from "@mui/material/Typography";
 import baseSkeletonProps from "@/components/SkeletonSwitcher/SkeletonSwitcher.config";
-import {ReactNode} from "react";
-import {documentToReactComponents} from "@contentful/rich-text-react-renderer";
-import {Document} from "@contentful/rich-text-types";
-import Link from "next/link";
-import Image from "next/image";
 import config from "@/config/sites";
-import stripTags from "@/utils/stripTags";
 import searchFormSchema, {SearchFormType} from "@/app/search/searchFormSchema";
 import {SubmitHandler, useForm} from "react-hook-form";
 import {yupResolver} from "@hookform/resolvers/yup";
 import HookFormTextField from "@/components/HookFormTextField/HookFormTextField";
-import {useRouter, useSearchParams} from "next/navigation";
+import {redirect, useSearchParams} from "next/navigation";
+import useSearch from "@/hooks/useSearch";
+import BlogPostSkeleton from "@/components/BlogPost/BlogPostSkeleton";
+import ItemAnimation from "@/components/ItemAnimation/ItemAnimation";
+import BlogPost from "@/components/BlogPost/BlogPost";
 
 const Search = () => {
   const searchParams = useSearchParams()
   const query = searchParams.get('query') ?? '';
+
+  const {
+    isLoading,
+    posts,
+    error,
+  } = useSearch('posts', query);
 
   const defaultVals: SearchFormType = {
     query: query,
@@ -32,33 +34,26 @@ const Search = () => {
 
   const {
     handleSubmit,
-    reset,
     control,
-    setError,
   } = useForm<SearchFormType>({
     resolver: yupResolver(searchFormSchema),
     defaultValues: defaultVals
   });
 
-  // const title = post ? `${config.meta.title} | Blog | ${post.fields.title}` : `${config.meta.title} | Blog`;
-  // const desc = post ? stripTags(post.fields.description as string) : '';
-  // const media = post?.fields.media;
-
   const onSubmit: SubmitHandler<SearchFormType> = (data) => {
-    console.log('submit', data);
+    redirect(`/search?query=${data.query}`)
   }
 
   return (
     <Container maxWidth={false} disableGutters>
-      {/*<Helmet>*/}
-      {/*  <title>Search</title>*/}
-      {/*  <meta property="description" content={desc}/>*/}
-      {/*  <meta property="keywords" content={post?.fields.keywords as string}/>*/}
-      {/*</Helmet>*/}
+      <Helmet>
+        <title>{`${config.meta.title} | Blog`}</title>
+        <meta property="description" content="Search website"/>
+        <meta property="keywords" content="search, site, site search" />
+      </Helmet>
 
       <SectionContainer styleName={styles.wrapper}>
         <form onSubmit={handleSubmit(onSubmit)}>
-          <h3>Search:</h3>
           <HookFormTextField
             name="query"
             control={control}
@@ -75,6 +70,60 @@ const Search = () => {
             Contact me
           </Button>
         </form>
+      </SectionContainer>
+
+      <SectionContainer styleName={styles.listItemWrapper}>
+        <Typography variant="h2" style={{ margin: '50px 0' }}>Results...</Typography>
+
+        {isLoading ? (
+          <>
+            <BlogPostSkeleton>
+              <Stack spacing={2} sx={{ margin: '25px 0 0' }}>
+                <Skeleton height={40} width="70%" variant="rectangular" {...{ ...baseSkeletonProps }} />
+              </Stack>
+
+              <Stack spacing={2} sx={{ margin: '25px 0 40px' }}>
+                <Skeleton width="90%" variant="rectangular" {...{ ...baseSkeletonProps }} />
+                <Skeleton variant="rectangular" {...{ ...baseSkeletonProps }} />
+                <Skeleton width="40%" variant="rectangular" {...{ ...baseSkeletonProps }} />
+              </Stack>
+
+              <Stack spacing={2} sx={{ margin: '20px 0' }}>
+                <Skeleton width="95%" variant="rectangular" {...{ ...baseSkeletonProps }} />
+                <Skeleton variant="rectangular" {...{ ...baseSkeletonProps }} />
+                <Skeleton variant="rectangular" {...{ ...baseSkeletonProps }} />
+                <Skeleton width="70%" variant="rectangular" {...{ ...baseSkeletonProps }} />
+              </Stack>
+            </BlogPostSkeleton>
+
+            <BlogPostSkeleton>
+              <Stack spacing={2} sx={{ margin: '25px 0 0' }}>
+                <Skeleton height={40} width="70%" variant="rectangular" {...{ ...baseSkeletonProps }} />
+              </Stack>
+
+              <Stack spacing={2} sx={{ margin: '25px 0 40px' }}>
+                <Skeleton width="90%" variant="rectangular" {...{ ...baseSkeletonProps }} />
+                <Skeleton variant="rectangular" {...{ ...baseSkeletonProps }} />
+                <Skeleton width="40%" variant="rectangular" {...{ ...baseSkeletonProps }} />
+              </Stack>
+
+              <Stack spacing={2} sx={{ margin: '20px 0 25px' }}>
+                <Skeleton width="95%" variant="rectangular" {...{ ...baseSkeletonProps }} />
+                <Skeleton variant="rectangular" {...{ ...baseSkeletonProps }} />
+                <Skeleton variant="rectangular" {...{ ...baseSkeletonProps }} />
+                <Skeleton width="20%" variant="rectangular" {...{ ...baseSkeletonProps }} />
+              </Stack>
+            </BlogPostSkeleton>
+          </>
+        ) : (
+          <>
+            {posts?.map(item => (
+              <ItemAnimation key={item.sys.id}>
+                <BlogPost post={item} />
+              </ItemAnimation>
+            ))}
+          </>
+        )}
       </SectionContainer>
     </Container>
 )
---
+++ src/hooks/useSearch.ts
@@ -0,0 +1,32 @@
+import client from "@/services/api";
+import useSWR from "swr";
+
+type PostTypes = 'blog' | 'work' | 'posts';
+
+const requestSearch = async (type: PostTypes, query: string) => {
+  try {
+    const res = await client.getEntries({
+      query: query
+    });
+
+    if(res.total) {
+      return res.items;
+    } else {
+      throw new Error('No content found...')
+    }
+  } catch (e) {
+    throw new Error(e as any);
+  }
+}
+
+const useSearch = (type: PostTypes, query: string) => {
+  const {data: posts, error, isLoading } = useSWR(`${type}/${query}`, (type: PostTypes) => requestSearch(type, query))
+
+  return {
+    isLoading,
+    posts,
+    error,
+  }
+}
+
+export default useSearch;
